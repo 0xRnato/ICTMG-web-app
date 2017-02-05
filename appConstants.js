b311f55eb2bf6d7f9b1d369cf7cd8473799d82c8
Renato Neto <rnato.netoo@gmail.com>
2017-02-05 17:11:02 -0200

    file for store constants values created

---
+++ appConstants.js
@@ -0,0 +1,7 @@
+//DATABASE CONSTANTS
+exports.DB_NAME = 'db_newUser';
+exports.DB_TABLE = 'tbl_user';
+exports.DB_HOST = 'localhost';
+exports.DB_PORT = 3306;
+exports.DB_USER = 'root';
+exports.DB_PASSWORD = 'root';
