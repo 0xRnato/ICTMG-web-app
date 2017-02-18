6c9a263a1b7297e0905cbda9fac5043dfbad8e2d
Renato Neto <rnato.netoo@gmail.com>
2017-02-18 16:31:06 -0200

    favicon added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 //Routes
 var index = require('./routes/index');
-var courses = require('./routes/courses');
+var course = require('./routes/course');
 var register = require('./routes/register');
 
 var app = express();
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Set routes
 app.use('/', index);
-app.use('/courses', courses);
+app.use('/course', course);
 app.use('/register', register);
 
 // catch 404 and forward to error handler
