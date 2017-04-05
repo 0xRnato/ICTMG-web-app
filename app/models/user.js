7862defac6ecbb71bdfa917284c6450f2f1d0f36
Arthur <vilaca.arthur@yahoo.com.br>
2017-04-04 21:59:50 -0300

    change table name

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 // define our model
 // user table example
 module.exports = (db) => {
-    return User = db.define('users', {
+    return User = db.define('user', {
         id: { type: 'serial', key: true },//Auto_increment key
         name: String,
         email: String,
--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,14 @@ module.exports = (app, db) => {
             res.json(users);
         });
     });
+
+    app.post('/api/users', (req, res) => {
+        User.find({}, (err, users) => {
+            if (err) res.send(err);
+            res.json(users);
+        });
+    });
+
     // Frontend routes ====================================
     // route to handle all angular requests
     app.get('*', (req, res) => {
