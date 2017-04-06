ba1153e8c1be354e83fa021880c37c19253cdfcd
Arthur <vilaca.arthur@yahoo.com.br>
2017-04-05 21:43:27 -0300

    courses routes, loading and add eslint for VS

---
+++ app/models/course.js
@@ -0,0 +1,36 @@
+
+module.exports = (db) => {
+    return Course = db.define('mdl_course', {
+        id: { type: 'serial', key: true },
+        category: Number,
+        sortorder: Number,
+        fullname: String,
+        shortname: String,
+        idnumber: String,
+        summary: String,
+        summaryformat: Number,
+        format: String,
+        showgrades: Number,
+        newsitems: Number,
+        startdate: Number,
+        enddate: Number,
+        marker: Number,
+        maxbytes: Number,
+        legacyfiles: Number,
+        showreports: Number,
+        visible: Number,
+        visibleold: Number,
+        groupmode: Number,
+        groupmodeforce: Number,
+        defaultgroupingid: Number,
+        lang: String,
+        calendartype: String,
+        theme: String,
+        timecreated: Number,
+        timemodified: Number,
+        requested: Number,
+        enablecompletion: Number,
+        completionnotify: Number,
+        cacherev: Number
+    });
+};
--- public/js/controllers/course.controller.js
+++ public/js/controllers/course.controller.js
@@ -1,3 +1,15 @@
-angular.module('CourseCtrl', []).controller('CourseController', function ($scope) {
+angular.module('CourseCtrl', []).controller('CourseController', function ($scope, $http) {
+    var init;
 
+    init = function() {
+        $scope.courses = [];
+        $http.get('api/courses')
+	  		.then(function sucessCallback(data) {
+                  $scope.courses = data.data;
+			}, function errorCallback(error){
+                console.log(error);
+			});
+    };
+
+    return init();
 });
