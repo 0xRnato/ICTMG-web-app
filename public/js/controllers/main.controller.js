219e4089304748501093b2610a38f5dc1cf1f3ae
Arthur <vilaca.arthur@yahoo.com.br>
2017-04-08 19:59:19 -0300

    change requests to services

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -2,21 +2,25 @@
     'use strict';
 
     angular
-        .module('MainCtrl', [])
+        .module('MainCtrl')
         .controller('MainController', MainController);
 
-    MainController.inject = ['$scope', '$http'];
-    function MainController($scope, $http) {
+    MainController.inject = ['$scope', '$http', 'Main'];
+    function MainController($scope, $http, Main) {
         let vm = this;
         activate();
 
         // Do something
         vm.calendar;
-        vm.gfr = 676;
+
+        vm.getDate = function(date) {
+            console.log(date);
+            return new Date(date);
+        }
 
         function activate() {
             vm.calendar = [];
-            $http.get('api/caledar')
+            Main.calendar.get()
                 .then(function sucessCallback(data) {
                     vm.calendar = data.data;
                 }, function errorCallback(error) {
