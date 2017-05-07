229ddc3eb724a900e5b26517761305df72b6ca26
Renato Neto <rnato.netoo@gmail.com>
2017-05-07 17:55:55 -0300

    admin calendar and notes

--- public/js/controllers/admin.controller.js
+++ public/js/controllers/admin.controller.js
@@ -5,13 +5,15 @@
         .module('app.admin')
         .controller('AdminController', AdminController)
 
-    AdminController.$inject = ['$log', 'UserService', '$rootScope'];
+    AdminController.$inject = ['$log', 'UserService', '$rootScope', 'AdminService'];
 
-    function AdminController($log, UserService, $rootScope) {
+    function AdminController($log, UserService, $rootScope, AdminService) {
         var vm = this;
         vm.users = [];
+        vm.notes = [];
+        vm.calendars = [];
 
-        vm.save = function(){
+        vm.saveUser = function(){
             UserService.save(angular.copy(vm.user))
                 .then(function sucessCallback(data){
                     vm.user = {};
@@ -19,11 +21,45 @@
                     alert('Usuário salvo com sucesso');
                 }, function errorCallback(error){
                     alert(error);
-                })
+                });
+        };
+
+        vm.saveNote = function(){
+            AdminService.saveNotes(angular.copy(vm.note))
+                .then(function sucessCallback(data){
+                    vm.note = {};
+                    vm.noteIsSetted = false;
+                    loadNotes();
+                    alert('Nota salva com sucesso');
+                }, function errorCallback(error){
+                    alert(error);
+                });
+        };
+
+        vm.saveCalendar = function(){
+            AdminService.saveCalendar(angular.copy(vm.calendar))
+                .then(function sucessCallback(data){
+                    vm.calendar = {};
+                    vm.calendarIsSetted = false;
+                    loadCalendars();
+                    alert('Evento salvo com sucesso');
+                }, function errorCallback(error){
+                    alert(error);
+                });
         };
 
         vm.setUser = function(_user){
             vm.user = _user;
+        }
+
+        vm.setNote = function(_note){
+            vm.note = _note;
+            vm.noteIsSetted = true;
+        }
+
+        vm.setCalendar = function(_calendar){
+            vm.calendar = _calendar;
+            vm.calendarIsSetted = true;
         }
 
         var loadUsers = function(){
@@ -35,11 +71,33 @@
             });
         }
 
+        var loadNotes = function(){
+            AdminService.loadNotes()
+                .then(function sucessCallback(data) {
+                    vm.notes = angular.copy(data.data);
+                }, function errorCallback(error) {
+                    $log.error('Error:' + error.status + ' - Message: ' + error.statusText);
+            });
+        }
+
+        var loadCalendars = function(){
+            AdminService.loadCalendar()
+                .then(function sucessCallback(data) {
+                    vm.calendars = angular.copy(data.data);
+                }, function errorCallback(error) {
+                    $log.error('Error:' + error.status + ' - Message: ' + error.statusText);
+            });
+        }
+
         activate();
 
         function activate() {
             vm.userSession = angular.copy($rootScope.userSession);
             loadUsers();
+            loadNotes();
+            loadCalendars();
+            vm.noteIsSetted = false;
+            vm.calendarIsSetted = false;
         }
     }
 })();
---
+++ public/js/services/admin.service.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.admin')
+        .factory('AdminService', AdminService);
+
+    AdminService.inject = ['$http', '$filter'];
+    function AdminService($http, $filter) {
+        var service = {
+            loadNotes: _loadNotes,
+            saveNotes: _saveNotes,
+            loadCalendar: _loadCalendar,
+            saveCalendar: _saveCalendar
+        };
+
+        return service;
+
+        function _loadNotes() {
+            return $http.get('/api/notes');
+        }
+
+        function _saveNotes(_note){
+            return $http.post('/api/notes', _note);
+        }
+
+        function _loadCalendar() {
+            return $http.get('/api/calendar');
+        }
+
+        function _saveCalendar(_calendar){
+            _calendar.startdate = $filter('date')(_calendar.startdate, 'yyyy-MM-dd hh:mm');
+            _calendar.enddate = $filter('date')(_calendar.enddate, 'yyyy-MM-dd hh:mm');
+            return $http.post('/api/calendar', _calendar);
+        }
+    }
+})();
